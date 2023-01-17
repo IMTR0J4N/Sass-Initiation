7d9bc9502a09ceb6b10ad472c668a6d30c6eb571
IMTR0J4NV2 <jjorgebeau@edenschool.fr>
2023-01-17 11:36:50 +0100

    Start code css library

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 const buildStyles = () => {
-    return src('index.scss')
+    return src('wallbird/**/*.scss')
     .pipe(sass())
     .pipe(dest('css'));
 }
 
 const watchTask = () => {
-    watch(['index.scss'], buildStyles)
+    watch(['wallbird/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
